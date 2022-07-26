867c6718bf4000908e1773594c647ff8445f6eb5
Mr_Dheer <22kavachdheer@gmail.com>
2022-07-26 10:42:07 +0530

    Targets

---
+++ src/Array_2D/SpirAL.java
@@ -0,0 +1,15 @@
+package Array_2D;
+
+import java.util.Scanner;
+
+public class SpirAL {
+    public static void main(String[] args) {
+
+     int [][] arr={{10,20,30}, {40,50,60}, {70,80,80}};
+
+
+
+
+
+    }
+}
